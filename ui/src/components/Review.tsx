cf81974ece70a6bae1e0f9fa7c42b0f8fea9a07b
sidgyl-anz <51866929+sidgyl-anz@users.noreply.github.com>
2025-03-08 16:14:46 +0900

    Update Review.tsx

--- ui/src/components/Review.tsx
+++ ui/src/components/Review.tsx
@@ -1,9 +1,37 @@
 import * as React from "react";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import ReactMarkdown from "react-markdown";
+import DOMPurify from "dompurify";
 import "../App.css";
+
+function MarkdownRenderer({ content }) {
+  if (!content) return null;
+
+  const sanitizedContent = DOMPurify.sanitize(content);
+
+  return (
+    <ReactMarkdown
+      components={{
+        p: ({ node, ...props }) => (
+          <Typography variant="body2" sx={{ marginBottom: "4px" }} {...props} />
+        ),
+        ol: ({ node, ...props }) => (
+          <ol style={{ margin: "0", paddingLeft: "20px" }} {...props} />
+        ),
+        ul: ({ node, ...props }) => (
+          <ul style={{ margin: "0", paddingLeft: "20px" }} {...props} />
+        ),
+        li: ({ node, ...props }) => (
+          <li style={{ marginBottom: "4px" }} {...props} />
+        ),
+      }}
+    >
+      {sanitizedContent}
+    </ReactMarkdown>
+  );
+}
 
 export default function Review({ response }) {
   return (
@@ -16,119 +44,28 @@ export default function Review({ response }) {
         Recommendations
       </Typography>
       <Grid container>
-        <React.Fragment>
-          <Stack direction="column" spacing={1} sx={{ width: "100%", mb: 5 }}>
-            <Typography
-              variant="body1"
-              sx={{
-                fontWeight: 400,
-                fontSize: "16px",
-                color: "text.secondary",
-              }}
-            >
-              Endocrinologist:
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ textAlign: "left", whiteSpace: "pre-wrap" }}
-            >
-              <ReactMarkdown
-              components={{
-                p: ({ node, ...props }) => <Typography variant="body2" sx={{ marginBottom: "0px" }} {...props} />,
-                ol: ({ node, ...props }) => <ol style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                ul: ({ node, ...props }) => <ul style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                li: ({ node, ...props }) => <li style={{ marginBottom: "0px" }} {...props} />,
-              }}
-            >
-              {response?.endocrinologistRecommendation}
-            </ReactMarkdown>
-            </Typography>
-          </Stack>
-        </React.Fragment>
-        <React.Fragment>
-          <Stack direction="column" spacing={1} sx={{ width: "100%", mb: 5 }}>
-            <Typography
-              variant="body1"
-              sx={{
-                fontWeight: 400,
-                fontSize: "16px",
-                color: "text.secondary",
-              }}
-            >
-              Dietician:
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ textAlign: "left", whiteSpace: "pre-wrap" }}
-            >
-              <ReactMarkdown
-              components={{
-                p: ({ node, ...props }) => <Typography variant="body2" sx={{ marginBottom: "0px" }} {...props} />,
-                ol: ({ node, ...props }) => <ol style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                ul: ({ node, ...props }) => <ul style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                li: ({ node, ...props }) => <li style={{ marginBottom: "0px" }} {...props} />,
-              }}
-            >  
-                {response?.dietitianRecommendation}</ReactMarkdown>
-            </Typography>
-          </Stack>
-        </React.Fragment>
-        <React.Fragment>
-          <Stack direction="column" spacing={1} sx={{ width: "100%", mb: 5 }}>
-            <Typography
-              variant="body1"
-              sx={{
-                fontWeight: 400,
-                fontSize: "16px",
-                color: "text.secondary",
-              }}
-            >
-              Fitness Trainer:
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ textAlign: "left", whiteSpace: "pre-wrap" }}
-            >
-             <ReactMarkdown
-              components={{
-                p: ({ node, ...props }) => <Typography variant="body2" sx={{ marginBottom: "0px" }} {...props} />,
-                ol: ({ node, ...props }) => <ol style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                ul: ({ node, ...props }) => <ul style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                li: ({ node, ...props }) => <li style={{ marginBottom: "0px" }} {...props} />,
-              }}
-            >
-                {response?.fitnessRecommendation}</ReactMarkdown>
-            </Typography>
-          </Stack>
-        </React.Fragment>
-        <React.Fragment>
-          <Stack direction="column" spacing={1} sx={{ width: "100%", mb: 5 }}>
-            <Typography
-              variant="body1"
-              sx={{
-                fontWeight: 400,
-                fontSize: "16px",
-                color: "text.secondary",
-              }}
-            >
-              Overall Recommendation:
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ textAlign: "left", whiteSpace: "pre-wrap" }}
-            >
-             <ReactMarkdown
-              components={{
-                p: ({ node, ...props }) => <Typography variant="body2" sx={{ marginBottom: "0px" }} {...props} />,
-                ol: ({ node, ...props }) => <ol style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                ul: ({ node, ...props }) => <ul style={{ margin: "0", paddingLeft: "20px" }} {...props} />,
-                li: ({ node, ...props }) => <li style={{ marginBottom: "0px" }} {...props} />,
-              }}
-            >
-              {response?.finalRecommendation}</ReactMarkdown>
-            </Typography>
-          </Stack>
-        </React.Fragment>
+        {[
+          { title: "Endocrinologist", content: response?.endocrinologistRecommendation },
+          { title: "Dietician", content: response?.dietitianRecommendation },
+          { title: "Fitness Trainer", content: response?.fitnessRecommendation },
+          { title: "Overall Recommendation", content: response?.finalRecommendation },
+        ].map((item, index) => (
+          <React.Fragment key={index}>
+            <Stack direction="column" spacing={1} sx={{ width: "100%", mb: 5 }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontWeight: 400,
+                  fontSize: "16px",
+                  color: "text.secondary",
+                }}
+              >
+                {item.title}:
+              </Typography>
+              <MarkdownRenderer content={item.content} />
+            </Stack>
+          </React.Fragment>
+        ))}
       </Grid>
     </div>
   );
